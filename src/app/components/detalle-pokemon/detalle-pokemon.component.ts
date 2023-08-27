2aebe19f8ac23b33aba0ee28ac8689e8c76e2f1a
Manuel Aranda <manuelarandacavieres@gmail.com>
2023-08-27 16:10:31 -0400

    Agrego efectos de hover

--- src/app/components/detalle-pokemon/detalle-pokemon.component.ts
+++ src/app/components/detalle-pokemon/detalle-pokemon.component.ts
@@ -13,10 +13,15 @@ export class DetallePokemonComponent implements OnInit{
 
   detallePokemon:Pokemon = {
     id : '',
+    type: '',
     name : '',
     weight : '',
     base_experience: '',
-    height: ''
+    height: '',
+    ability: '',
+    hp : '',
+    attack : '',
+    defense : '',
   };
 
   constructor(private activatedRoute: ActivatedRoute, private pokeApiService: PokeApiService){}
@@ -30,14 +35,19 @@ export class DetallePokemonComponent implements OnInit{
 
   async cargarPokemon(id:string){
     const resp = await this.pokeApiService.getByID(id);
-    console.log(resp);
+    //console.log(resp);
 
     this.detallePokemon= {
       id : id,
+      type: resp.types[0].type.name,
       name : resp.name,
       weight : resp.weight,
       base_experience: resp.base_experience,
-      height: resp.height
+      height: resp.height,
+      ability: resp.abilities[0].ability.name,
+      hp: resp.stats[0].base_stat,
+      attack: String(resp.stats[1].base_stat),
+      defense: String(resp.stats[2].base_stat)
     };
   }
 }
